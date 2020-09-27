46f25331e0c825caa26a0df2cc03df2168a32914
whoisnegi <k.negi791@gmail.com>
2020-09-27 15:14:32 +0530

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { CssBaseline } from "@material-ui/core";
 import { BrowserRouter } from "react-router-dom";
-
 import Routes from "./Routes";
+
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes />
-      <CssBaseline />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes />
+            <CssBaseline />
+        </BrowserRouter>
+    );
 };
 
 export default App;
