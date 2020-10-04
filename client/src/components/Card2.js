daf14eae55e2b5458c1c2ca4b494e22925c9c5f1
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-10-04 12:54:20 +0530

    Packages added

--- client/src/components/Card2.js
+++ client/src/components/Card2.js
@@ -1,50 +1,30 @@
-import React, { Fragment } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardMedia from "@material-ui/core/CardMedia";
-import Image from "material-ui-image";
-import { HoverMode } from "react-particles-js";
-import { LinearScale } from "@material-ui/icons";
-import ZoomOutMapIcon from "@material-ui/icons/ZoomOutMap";
-import {
-  GridList,
-  GridListTile,
-  Dialog,
-  IconButton,
-  Paper,
-} from "@material-ui/core";
-
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-    borderRadius: 0,
-    margin: "10px",
-    position: "relative",
-  },
-});
+import React, { Suspense } from "react";
+import FavoriteOutlinedIcon from "@material-ui/icons/FavoriteOutlined";
+import AddCommentIcon from "@material-ui/icons/AddComment";
+import { IconButton } from "@material-ui/core";
+import ReactImageAppear from "react-image-appear";
+import LoaderImage from "../images/placeholder.gif";
 
 export default function ImgMediaCard({ post }) {
-  const classes = useStyles();
-
   return (
-    // <Paper className={classes.root} elevation={3}>
-    //   <Image
-    //     className="photoEffects"
-    //     src={post.image.imageUrl}
-    //     alt="UserImages"
-    //   />
-    // </Paper>
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="200"
-          image={post.image.imageUrl}
-          title="Contemplative Reptile"
-        />
-      </CardActionArea>
-    </Card>
+    <Suspense>
+      <IconButton>
+        <div className="container">
+          <ReactImageAppear
+            src={post.image.imageUrl}
+            alt="Avatar"
+            className="image"
+            style={{ width: "100%" }}
+            loader={LoaderImage}
+          />
+          <div className="middle">
+            <div className="text">
+              <FavoriteOutlinedIcon /> {"Likes"}
+              <AddCommentIcon /> {"Comments"}
+            </div>
+          </div>
+        </div>
+      </IconButton>
+    </Suspense>
   );
 }
